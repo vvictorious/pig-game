43d34ca232ab33d4733390e14955d81f5f6404fa
vvictorious <vvictorious30@gmail.com>
2019-05-07 12:36:21 -0600

    the dice in the dom is correctly changing randomly with every click

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ let activePlayer = 0;
 const newButton = document.querySelector('.btn-new');
 const holdButton = document.querySelector('.btn-hold');
 const rollButton = document.querySelector('.btn-roll');
+
+
+
+rollButton.addEventListener('click', () => {
+	let dice = Math.floor(Math.random() * 6) + 1;
+	let diceDom = document.querySelector('.dice');
+	diceDom.src = 'dice-' + dice + '.png'
+})
