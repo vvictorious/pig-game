7e288cc30c3f47989a40e51820d08c052ba16e20
vvictorious <vvictorious30@gmail.com>
2019-05-07 12:26:21 -0600

    finally starting the js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log('Hello World');
+
+let scores = [0, 0];
+let roundScore = 0;
+let activePlayer = 0;
+
+const newButton = document.querySelector('.btn-new');
+const holdButton = document.querySelector('.btn-hold');
+const rollButton = document.querySelector('.btn-roll');
