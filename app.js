4196b90e7fbf89a3b029db1d6b896079b6097b61
vvictorious <vvictorious30@gmail.com>
2019-05-08 13:42:15 -0600

    the ui changes when the player changes

--- app.js
+++ app.js
@@ -24,7 +24,12 @@ rollButton.addEventListener('click', () => {
 	} else {
 		//this is where I change the active player
 		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-		roundScore = 0;
+		roundScore = 0;  //reset the current score
+		document.getElementById('current-0').textContent = '0'; //change the dom
+		document.getElementById('current-1').textContent = '0';
+		document.querySelector('.player-0-panel').classList.toggle('active'); 
+		document.querySelector('.player-1-panel').classList.toggle('active');
+			
 	}
 });
 
