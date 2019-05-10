0ea93b2795e85e34a4455f1fd1ba021fdf5563dc
vvictorious <vvictorious30@gmail.com>
2019-05-10 12:04:47 -0600

    added some if statements so the game can not continue after a winner is declared

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('Hello World');
 
 
 //these are going to be the scores and active players
-	var scores, roundScore, activePlayer;
+	let scores, roundScore, activePlayer, gamePlaying;
 	init();
 
 
@@ -14,14 +14,16 @@ const rollButton = document.querySelector('.btn-roll');
 
 
 rollButton.addEventListener('click', () => {
-	let dice = Math.floor(Math.random() * 6) + 1;
-	let diceDom = document.querySelector('.dice');
-	diceDom.src = 'dice-' + dice + '.png'
-	if (dice !== 1) {
-		roundScore += dice;
-		document.querySelector('#current-' + activePlayer).textContent = roundScore;
-	} else { //if ya roll a 1
-		nextPlayer();
+	if (gamePlaying === true) {
+		let dice = Math.floor(Math.random() * 6) + 1;
+		let diceDom = document.querySelector('.dice');
+		diceDom.src = 'dice-' + dice + '.png'
+		if (dice !== 1) {
+			roundScore += dice;
+			document.querySelector('#current-' + activePlayer).textContent = roundScore;
+		} else { //if ya roll a 1
+			nextPlayer();
+		}
 	}
 });
 
@@ -33,19 +35,21 @@ rollButton.addEventListener('click', () => {
 //then change the active class and active player to the next player
 //finally when the score reaches a certain 
 holdButton.addEventListener('click', () => {
-	//hold the overall score in a variable
-	scores[activePlayer] = scores[activePlayer] + roundScore;
-	//update the ui
-	document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
-	//see if they won the game 
-	if(scores[activePlayer] >= 15){
-		document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
-		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-	} else {
-		nextPlayer();
+	if (gamePlaying === true) {
+		//hold the overall score in a variable
+		scores[activePlayer] = scores[activePlayer] + roundScore;
+		//update the ui
+		document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+		//see if they won the game 
+		if(scores[activePlayer] >= 50){
+			document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
+			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+			document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+			gamePlaying = false;
+		} else {
+			nextPlayer();
+		}
 	}
-
 })
 
 newButton.addEventListener('click', init);
@@ -65,7 +69,8 @@ function nextPlayer(){
 function init(){
 	scores = [0, 0];
 	roundScore = 0;
-	activePlayer = 0;		
+	activePlayer = 0;
+	gamePlaying = true;		
 	document.getElementById('score-0').textContent = '0';
 	document.getElementById('score-1').textContent = '0';
 	document.getElementById('current-0').textContent = '0';
